eb60a7bcab35fcd29d8c4d1aa3585ceb8a56f19c
John Carlo Cheng Roa <61899688+johncarlochengroa@users.noreply.github.com>
2025-09-17 13:03:30 +0800

    Version 4

---
+++ InventoryOOP/src/ItemManager.java
@@ -0,0 +1,98 @@
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class ItemManager {
+
+    static Scanner input = new Scanner(System.in);
+
+    static String[] addItem(){
+        String[] properties = new String[7];
+        properties[0] = "0";
+
+        System.out.println("\nInventoryOOP: Add New Item");
+        System.out.print("Enter the name of the item you want to add: ");
+        properties[1] = input.nextLine();
+        System.out.print("Enter the description of the item you want to add: ");
+        properties[2] = input.nextLine();
+        System.out.print("Enter the quantity of the item you want to add: ");
+        properties[3] = input.nextLine();
+
+        if (properties[2].toUpperCase(Locale.ROOT).equals("FOOD")){
+            properties[0] = "1";
+            System.out.print("Enter the calories of the food item you want to add: ");
+            properties[4] = input.nextLine();
+            System.out.print("Is your food item dangerous?\n[Y] - Yes | [N] - No\n>> ");
+            String isDangerous = input.nextLine();
+            if (isDangerous.toUpperCase(Locale.ROOT).equals("Y")) {
+                properties[5] = "Yes";
+            }
+            else if (isDangerous.toUpperCase(Locale.ROOT).equals("N")) {
+                properties[5] = "No";
+            }
+            else {
+                properties[5] = "";
+            }
+            System.out.print("Is your food item vegan?\n[Y] - Yes | [N] - No\n>> ");
+            String isVegan = input.nextLine();
+            if (isVegan.toUpperCase(Locale.ROOT).equals("Y")) {
+                properties[6] = "Yes";
+            }
+            else if (isVegan.toUpperCase(Locale.ROOT).equals("N")) {
+                properties[6] = "No";
+            }
+            else {
+                properties[6] = "";
+            }
+        }
+
+        return properties;
+    }
+
+    static void printResults(String[] itemProperties){
+        if (itemProperties[1].isEmpty()) {
+            System.out.println("Missing Required Fields: Item Name");
+            itemProperties[0] = "";
+        }
+        if (itemProperties[2].isEmpty()) {
+            System.out.println("Missing Required Fields: Item Description");
+            itemProperties[0] = "";
+        }
+        if (itemProperties[3].isEmpty()) {
+            System.out.println("Missing Required Fields: Item Quantity");
+            itemProperties[0] = "";
+        }
+        if (itemProperties[0].equals("0")) {
+            Item item = new Item(itemProperties[1],itemProperties[2],Integer.parseInt(itemProperties[3]));
+            System.out.println();
+            System.out.println("Name of Item: " + item.getItemName());
+            System.out.println("Description of Item: " + item.getItemDescription());
+            System.out.println("Quantity of Item: " + item.getItemQuantity());
+        }
+        else if (itemProperties[0].equals("1")) {
+            Food food = new Food(itemProperties[1],itemProperties[2],Integer.parseInt(itemProperties[3]),itemProperties[4],itemProperties[5],itemProperties[6]);
+            System.out.println();
+            System.out.println("Name of Food: " + food.getItemName());
+            System.out.println("Description of Food: " + food.getItemDescription());
+            System.out.println("Quantity of Food: " + food.getItemQuantity());
+            if(!Objects.equals(food.getCalories(), "")){
+                System.out.println("Calories of Food: " + food.getCalories());
+            }
+            if(!Objects.equals(food.getIsDangerous(), "")){
+                System.out.println("Is the Food Dangerous?: " + food.getIsDangerous());
+            }
+            if(!Objects.equals(food.getIsVegan(), "")){
+                System.out.println("Is the Food Vegan?: " + food.getIsVegan());
+            }
+        }
+    }
+
+    static void displayMenu(boolean alreadyPopulated){
+        if (alreadyPopulated) {
+            System.out.print("\nInventory Action\n[1] - Identify Item\n[2] - Delete Item\n[3] - Exit\n>> ");
+        }
+        else {
+            System.out.print("\nInventory Action\n[1] - Add Item\n[2] - Exit\n>> ");
+        }
+    }
+}
--- InventoryOOP/src/Main.java
+++ InventoryOOP/src/Main.java
@@ -3,8 +3,8 @@
  * A Project that demonstrates a basic OOP approach.
  *
  * Project by John Carlo E. Cheng Roa
- * Version 3
- * September 17, 2025 - 6:56 AM
+ * Version 4
+ * September 17, 2025 - 1:03 PM
  */
 
 import java.util.Locale;
@@ -12,91 +12,52 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
-    static String[] addItem(){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        String[] properties = new String[7];
-        properties[0] = "0";
+        String[] item = new String[7];
+        boolean alreadyPopulated = false;
 
         System.out.println("\nInventoryOOP");
-        System.out.print("Enter the name of the item you want to add: ");
-        properties[1] = input.nextLine();
-        System.out.print("Enter the description of the item you want to add: ");
-        properties[2] = input.nextLine();
-        System.out.print("Enter the quantity of the item you want to add: ");
-        properties[3] = input.nextLine();
 
-        if (properties[2].toUpperCase(Locale.ROOT).equals("FOOD")){
-            properties[0] = "1";
-            System.out.print("Enter the calories of the food item you want to add: ");
-            properties[4] = input.nextLine();
-            System.out.print("Is your food item dangerous?\n[Y] - Yes | [N] - No\n>> ");
-            String isDangerous = input.nextLine();
-            if (isDangerous.toUpperCase(Locale.ROOT).equals("Y")) {
-                properties[5] = "Yes";
-            }
-            else if (isDangerous.toUpperCase(Locale.ROOT).equals("N")) {
-                properties[5] = "No";
+        label:
+        while (true) {
+            if (alreadyPopulated) {
+                ItemManager.displayMenu(true);
             }
             else {
-                properties[5] = "";
+                ItemManager.displayMenu(false);
             }
-            System.out.print("Is your food item vegan?\n[Y] - Yes | [N] - No\n>> ");
-            String isVegan = input.nextLine();
-            if (isVegan.toUpperCase(Locale.ROOT).equals("Y")) {
-                properties[6] = "Yes";
-            }
-            else if (isVegan.toUpperCase(Locale.ROOT).equals("N")) {
-                properties[6] = "No";
+            String userChoice = input.nextLine();
+            if (alreadyPopulated) {
+                switch (userChoice) {
+                    case "1":
+                        ItemManager.printResults(item);
+                        break;
+                    case "2":
+                        for (int i = 0; i < 7; i++) {
+                            item[i] = "";
+                        }
+                        alreadyPopulated = false;
+                        System.out.println("Item deleted successfully");
+                        break;
+                    case "3":
+                        break label;
+                    default:
+                        break;
+                }
             }
             else {
-                properties[6] = "";
+                switch (userChoice) {
+                    case "1":
+                        item = ItemManager.addItem();
+                        alreadyPopulated = true;
+                        break;
+                    case "2":
+                        break label;
+                    default:
+                        break;
+                }
             }
         }
-
-        return properties;
-    }
-
-    static void printResults(String[] itemProperties){
-        if (itemProperties[1].isEmpty()) {
-            System.out.println("Missing Required Fields: Item Name");
-            itemProperties[0] = "";
-        }
-        if (itemProperties[2].isEmpty()) {
-            System.out.println("Missing Required Fields: Item Description");
-            itemProperties[0] = "";
-        }
-        if (itemProperties[3].isEmpty()) {
-            System.out.println("Missing Required Fields: Item Quantity");
-            itemProperties[0] = "";
-        }
-        if (itemProperties[0].equals("0")) {
-            Item item = new Item(itemProperties[1],itemProperties[2],Integer.parseInt(itemProperties[3]));
-            System.out.println();
-            System.out.println("Name of Item: " + item.getItemName());
-            System.out.println("Description of Item: " + item.getItemDescription());
-            System.out.println("Quantity of Item: " + item.getItemQuantity());
-        }
-        else if (itemProperties[0].equals("1")) {
-            Food food = new Food(itemProperties[1],itemProperties[2],Integer.parseInt(itemProperties[3]),itemProperties[4],itemProperties[5],itemProperties[6]);
-            System.out.println();
-            System.out.println("Name of Food: " + food.getItemName());
-            System.out.println("Description of Food: " + food.getItemDescription());
-            System.out.println("Quantity of Food: " + food.getItemQuantity());
-            if(!Objects.equals(food.getCalories(), "")){
-                System.out.println("Calories of Food: " + food.getCalories());
-            }
-            if(!Objects.equals(food.getIsDangerous(), "")){
-                System.out.println("Is the Food Dangerous?: " + food.getIsDangerous());
-            }
-            if(!Objects.equals(food.getIsVegan(), "")){
-                System.out.println("Is the Food Vegan?: " + food.getIsVegan());
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-            String[] itemProperties = addItem();
-            printResults(itemProperties);
     }
 }
